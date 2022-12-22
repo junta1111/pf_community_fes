2de48e095abe336633f73a99a543e2ce80fc8d73
junta1111 <morijun1111@gmail.com>
2022-12-22 14:22:31 +0000

    pf_fes_4

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :goods
+  devise_for :comments
+  devise_for :communities
+  devise_for :admins
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
