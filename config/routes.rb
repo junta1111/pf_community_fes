b697f50f11254cddf9bdaa24f1dc1621e1091aa2
junta1111 <morijun1111@gmail.com>
2022-12-25 13:17:11 +0000

    pf_fes_5

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
+
+  devise_for :users,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+
   devise_for :goods
   devise_for :comments
   devise_for :communities
-  devise_for :admins
-  devise_for :users
+  #devise_for :admins
+  #devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
