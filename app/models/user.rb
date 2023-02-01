809ab85f0dbfeab6beabfa23088a5c19878a6b6c
junta1111 <morijun1111@gmail.com>
2023-02-01 13:47:18 +0000

    pf_fes_11

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,8 @@ class User < ApplicationRecord
   has_many :favorite_events, dependent: :destroy
   has_one_attached :profile_image
 
+  enum sex: { women: 0, men: 1 }
+
   def get_profile_image(width, height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
